7eddd3be92beee0fa4a27dce16b058b2bf83beea
NeonHayford <hayford.lumorvi@gmail.com>
2023-07-25 13:05:09 +0000

    commit change

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(category)
+admin.site.register(menu)
+admin.site.register(cart)
+admin.site.register(booking)
+admin.site.register(delivery)
